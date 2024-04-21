603ebf88a5e3b02d8a303c49d4bf035290e2c159
sceiiya <meetscheidj@gmail.com>
2024-04-21 17:59:35 +0800

    update developmetn practice

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,10 +1,14 @@
-import Image from 'next/image'
+import Link from "next/link";
+import ProductCard from "./components/ProductCard";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Konnichiwa Sekai ie</h1>
-      <h1>Konbanha</h1>
+        <a href="/users">Boom boom USerSz@!</a>
+        <Link href="/users" className="bold text-lg"> Ded Backudan Users </Link>
+        <ProductCard />
+      <h2>Konbanha</h2>
     </main>
   )
 }
