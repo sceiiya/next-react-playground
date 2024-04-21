f7cb88362d1b1987e257e4db087ae78b46a7c27a
sceiiya <meetscheidj@gmail.com>
2024-04-21 17:58:47 +0800

    add client component

---
+++ app/app/components/AddToCart.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+const AddToCart = () => {
+    let x = 8
+  return (
+    <>
+        <button onClick={() => {console.log(x)}} >Click moe</button>
+    </>
+  )
+}
+
+export default AddToCart
