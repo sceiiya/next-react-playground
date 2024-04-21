d936e3d716529581c892212449235244e7c9a892
sceiiya <meetscheidj@gmail.com>
2024-04-21 17:59:23 +0800

    add dummy fetch to users component

--- app/app/users/page.tsx
+++ app/app/users/page.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
 
-const UserPage = () => {
+  interface User {
+    id: number;
+    name: string;
+    username: string;
+  }
+
+const UserPage = async () => {
+  const res = await fetch('https://drive.google.com/file/d/1opSBDuvQqK46X4zeMBYR_ezAe0odbzsU/view?usp=drive_link')
+  console.log(res)
+  const users = res.json()
   return (
-    <div>UserPage</div>
+    <div>{users}</div>
   )
 }
 
